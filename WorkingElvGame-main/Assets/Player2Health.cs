6004b062248d973f5a44f537982cd30bf65dd4f6
Bungus07 <146829051+Bungus07@users.noreply.github.com>
2025-02-12 11:31:15 +0000

    GameFinal

--- WorkingElvGame-main/Assets/Player2Health.cs
+++ WorkingElvGame-main/Assets/Player2Health.cs
@@ -1,22 +1,33 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 public class Player2Health : MonoBehaviour
 {
     public Slider healthSlider;
     public int maxHealth = 100;
     private int currentHealth;
+
     void Start()
     {
         currentHealth = maxHealth;
         healthSlider.maxValue = maxHealth;
         healthSlider.value = currentHealth;
     }
+
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
         currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
         healthSlider.value = currentHealth;
+
+        if (currentHealth == 0)
+        {
+            // Load the "Player1Win" scene when Player2Health reaches 0
+            SceneManager.LoadScene("Player1Win");
+        }
     }
+
     public void Heal(int amount)
     {
         currentHealth += amount;
